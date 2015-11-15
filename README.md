b9ff6325d6a093986adaf174f2e06f2c0bfb7060
lummie <lummie.uk@gmail.com>
2015-11-15 23:00:22 +0000

    First Version of dns-dojo

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
-# dns-dodo
-a DNS sub-domain IP updater for Digital Ocean
+<div style="margin:0px auto; width:380px;">
+<pre style="font-size:0.7em; background-color:#fff;color:#99cc00; line-height: 75%;font-weight:bold;">
+                   .`.....
+                :::;++:;;+`
+              .:+@@@@@@:@+`.``
+             ::+@@;:.'@@;';'#,;.
+            .:+@;;;;;,.#':@
+            :+@,;'#',;@++#      ..
+            ;+,`#`      @'     `..    ``````
+           .'#;#        ...'++:`,: `.......```
+           ,+@'         ..'::;+`.,`.,:.,,,,.```
+           .+##   :#+#  ..,;@#+;.,.,;;;:,;;,,.``
+           ,+#+. ;`,;;#....+@.`.:,:;;;;..,;;:,```
+           ,+@,.`+;,@:+'.,;,;::::;;;;;,``.;;;,.```
+           ,#+...+:@@;+@.:::::;;;;;;;;,..,;;;;,.``
+           :'+#..@+;#+#;,;:';;;+';;;;;::,:;;';,```
+          `.++;;::@++;#::::';;;;;';''';;;;;'';:.`.
+            :+++;:;@@+;:::;;;;;;;;'''''''''''';,..
+            .'+;;'';;;:::#;;;;;;'''''''''''''';,`.
+             ;+':+.:;;:;++++++++'''''''''''''';,..
+              :'+'::'+''++++++++++'''''''''''';,``
+               .+@+++++#++++++++#++''''''''''';,.`
+                .+@@@++@@+` `:+++;++'''''''''';,.
+                  ;@@@@@@+    .++;;'#''''''''';.
+                   ;@@@###     ,#+++##''''''+':`
+                    '@@++++`    :###@;+''''+';.
+                    :#@;;+++:   `.,,` `+++#++.
+                    :++;;::;,;         ###++.
+                    :++;,,,,...        ,++:`
+                   .+++;,,....,.        ,`
+   .. .     ...,..;'+++:,.....``.
+  ..:.` `` ....,;++++++;,......`
+ .,   .`. ..;:':;++++++;,....... `
+ `...`....:'++.`.;++++'+........
+  ;,..;;.:'++:`,:::++++++,...... .
+ .';,.:.;.++++`,,,:@#++++':.....
+ `;++,,:.  +  .,,,;@++++++++,,..
+  ,;;:''.. : .,,,,:++++++##@@,..
+    .+++,. ``,,,,,;:+++++@@@@,..
+     @@; ,...,,,::;;+++#@@@@@,.
+     #@+ ,::,,,,,;;,###@@@@@@,`
+     +@@.:;,,,,,:;'+@@@@@@@@+.
+      +@@';:::;;'':@@@@@@@@@,
+       .@@;+'++'';@@@@@@@@@@
+          @@@++@@@+@@##@''+'
+           ;@@@###@+':   ';'     ,`
+              @##       ,'':    +';
+              #';`+:+::#;+` `:;;+:+;.
+           ..:;:':;'###'#@:'';::':;:+
+ ..::::',;::;;;#:;::::::.:';'#++++':,
+:::::;:;;;':;+:::,..;:;+:;''+;'+,'.::::;::,.
+::::''';:;#'##:;'++'','':`::,''''#::::::::::::..
+:::;:;:;:;::;;:+#'':;;;::::;:::;:::::;:::::::;;..
+..:::::;::;;,;'':;,,,.;;:;;:::;::::::::::::::::.
+    ;:::;;:;:::;;:::,'.;:;;;;;:::::::::::::..
+        ..;;:::;;;'#+';:::.
+
+
+      _                     _           _
+     | |                   | |         | |
+   __| |_ __  ___ ______ __| | ___   __| | ___
+  / _` | '_ \/ __|______/ _` |/ _ \ / _` |/ _ \
+ | (_| | | | \__ \     | (_| | (_) | (_| | (_) |
+  \__,_|_| |_|___/      \__,_|\___/ \__,_|\___/
+</pre>
+</div>
+###DNS-Digital Ocean DO - a DNS sub-domain IP updater for Digital Ocean
+
+dns-dodo's main purpose is to update a single dns 'A' record to the public IP address of the system dns-dojo is run on.
+This is very similar to the dynamic dns clients that you can download for no-ip, dyndns, etc. but
+dns-dodo allows you to use your existing Digital Ocean account for this service.
+
+In addition dns-dodo allows you to show the public ip, and show the current dns records (all types) associated with a domain on your digital ocean account.
+
+
+### Who / What is Digital Ocean
+Digital Ocean, https://www.digitalocean.com/ provide a Simple Cloud Infrastructure for Developers. You can setup a virtual server in seconds (about 20) and all are SSD based so are responsive.
+They are affordable for the casual developer too...
+
+### Setting up on the Digital Ocean side
+1) Add a A record to an existing domain with the name set to the sub-domain you would like to use and initially set it to your droplets IP address so you can validate that dns-dodo works.
+2) Get your Personal Access Token (PAT) that provides you with the authentication to talk to your Digital Ocean account. **NOTE** If someone gets hold of your PAT they have full api access to create/delete droplets and make your life a misery so please be careful.
+You can get this from the applications page https://cloud.digitalocean.com/settings/applications
+
+
+
+#### Usage
+
+Get Help
+
+    dns-dodo help [command]
+
+----
+
+Show the external IP address using the default External IP Service
+
+    dns-dodo show-ip
+
+Show the external IP address using an alternate IP service
+
+    dns-dodo --ext-ip=https://api.ipify.org show-ip
+
+----
+
+
+Show the current DNS entries for a specific domain on your Digital Ocean Account
+
+    dns-dodo show-dns --pat=[your-long-personal-access-token-here] --domain=[domain-name-to-update-dns-record-for]
+
+Filter the current DNS entries for a specific sub-domain and domain on your Digital Ocean Account
+
+    dns-dodo show-dns --name=home --pat=[your-pat] --domain=[domain-name]
+
+Filter the current DNS entries for a specific record type and domain on your Digital Ocean Account
+
+    dns-dodo show-dns --type=A --pat=[your-pat] --domain=[domain-name]
+
+
+----
+
+Update the DNS A record for a specific domain and sub-domain to the External IP Adddress
+
+    dns-dodo update-dns --pat=[your-pat] --domain=[domain-name] --sub-domain=home
+
+
+----
---
+++ dns-dodo.go
@@ -0,0 +1,399 @@
+/*
+dns-dodo's main purpose is to update a single dns 'A' record to the public IP address of the system dns-dojo is run on.
+*/
+package main
+
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+	"github.com/digitalocean/godo"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var externalIPService string = "http://myexternalip.com/raw"
+
+type dnsDodo struct {
+	DOPersonalAccessToken string
+	ExternalIPServiceUrl  string
+	OAuthClient           *http.Client
+	client                *godo.Client
+}
+
+// displays the supplied message and exits the application with an error level of 1
+func exitWithError(message string) {
+	fmt.Println(message)
+	os.Exit(1)
+}
+
+// Create a DNS-dodo instance.
+func NewDnsDoDO() *dnsDodo {
+	d := dnsDodo{}
+	return &d
+}
+
+// Displays the About dns-dodo text
+func (d *dnsDodo) About() {
+	fmt.Println("")
+	fmt.Println("                   .`.....")
+	fmt.Println("                :::;++:;;+`")
+	fmt.Println("              .:+@@@@@@:@+`.``")
+	fmt.Println("             ::+@@;:.'@@;';'#,;.")
+	fmt.Println("            .:+@;;;;;,.#':@")
+	fmt.Println("            :+@,;'#',;@++#      ..")
+	fmt.Println("            ;+,`#`      @'     `..    ``````")
+	fmt.Println("           .'#;#        ...'++:`,: `.......```")
+	fmt.Println("           ,+@'         ..'::;+`.,`.,:.,,,,.```")
+	fmt.Println("           .+##   :#+#  ..,;@#+;.,.,;;;:,;;,,.``")
+	fmt.Println("           ,+#+. ;`,;;#....+@.`.:,:;;;;..,;;:,```")
+	fmt.Println("           ,+@,.`+;,@:+'.,;,;::::;;;;;,``.;;;,.```")
+	fmt.Println("           ,#+...+:@@;+@.:::::;;;;;;;;,..,;;;;,.``")
+	fmt.Println("           :'+#..@+;#+#;,;:';;;+';;;;;::,:;;';,```")
+	fmt.Println("          `.++;;::@++;#::::';;;;;';''';;;;;'';:.`.")
+	fmt.Println("            :+++;:;@@+;:::;;;;;;;;'''''''''''';,..")
+	fmt.Println("            .'+;;'';;;:::#;;;;;;'''''''''''''';,`.")
+	fmt.Println("             ;+':+.:;;:;++++++++'''''''''''''';,..")
+	fmt.Println("              :'+'::'+''++++++++++'''''''''''';,``")
+	fmt.Println("               .+@+++++#++++++++#++''''''''''';,.`")
+	fmt.Println("                .+@@@++@@+` `:+++;++'''''''''';,.")
+	fmt.Println("                  ;@@@@@@+    .++;;'#''''''''';.")
+	fmt.Println("                   ;@@@###     ,#+++##''''''+':`")
+	fmt.Println("                    '@@++++`    :###@;+''''+';.")
+	fmt.Println("                    :#@;;+++:   `.,,` `+++#++.")
+	fmt.Println("                    :++;;::;,;         ###++.")
+	fmt.Println("                    :++;,,,,...        ,++:`")
+	fmt.Println("                   .+++;,,....,.        ,`")
+	fmt.Println("   .. .     ...,..;'+++:,.....``.")
+	fmt.Println("  ..:.` `` ....,;++++++;,......`")
+	fmt.Println(" .,   .`. ..;:':;++++++;,....... `")
+	fmt.Println(" `...`....:'++.`.;++++'+........")
+	fmt.Println("  ;,..;;.:'++:`,:::++++++,...... .")
+	fmt.Println(" .';,.:.;.++++`,,,:@#++++':.....")
+	fmt.Println(" `;++,,:.  +  .,,,;@++++++++,,..")
+	fmt.Println("  ,;;:''.. : .,,,,:++++++##@@,..")
+	fmt.Println("    .+++,. ``,,,,,;:+++++@@@@,..")
+	fmt.Println("     @@; ,...,,,::;;+++#@@@@@,.")
+	fmt.Println("     #@+ ,::,,,,,;;,###@@@@@@,`")
+	fmt.Println("     +@@.:;,,,,,:;'+@@@@@@@@+.")
+	fmt.Println("      +@@';:::;;'':@@@@@@@@@,")
+	fmt.Println("       .@@;+'++'';@@@@@@@@@@")
+	fmt.Println("          @@@++@@@+@@##@''+'")
+	fmt.Println("           ;@@@###@+':   ';'     ,`")
+	fmt.Println("              @##       ,'':    +';")
+	fmt.Println("              #';`+:+::#;+` `:;;+:+;.")
+	fmt.Println("           ..:;:':;'###'#@:'';::':;:+")
+	fmt.Println(" ..::::',;::;;;#:;::::::.:';'#++++':,")
+	fmt.Println(":::::;:;;;':;+:::,..;:;+:;''+;'+,'.::::;::,.")
+	fmt.Println("::::''';:;#'##:;'++'','':`::,''''#::::::::::::..")
+	fmt.Println(":::;:;:;:;::;;:+#'':;;;::::;:::;:::::;:::::::;;..")
+	fmt.Println("..:::::;::;;,;'':;,,,.;;:;;:::;::::::::::::::::.")
+	fmt.Println("    ;:::;;:;:::;;:::,'.;:;;;;;:::::::::::::..")
+	fmt.Println("        ..;;:::;;;'#+';:::.")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("      _                     _           _")
+	fmt.Println("     | |                   | |         | |")
+	fmt.Println("   __| |_ __  ___ ______ __| | ___   __| | ___")
+	fmt.Println("  / _` | '_ \\/ __|______/ _` |/ _ \\ / _` |/ _ \\")
+	fmt.Println(" | (_| | | | \\__ \\     | (_| | (_) | (_| | (_) |")
+	fmt.Println("  \\__,_|_| |_|___/      \\__,_|\\___/ \\__,_|\\___/")
+	fmt.Println("")
+	fmt.Println("               dns Digital Ocean do  ")
+	fmt.Println("")
+	fmt.Println(" Dynamic DNS sub-domain updater for Digital Ocean  ")
+	fmt.Println(" by lummie  - http://lummie.github.io ")
+	fmt.Println("")
+}
+
+// Returns the public ip address as returned by the specified ExternalIPServiceUrl
+func (d *dnsDodo) getExternalIP() string {
+	// check IP service URL exists
+	if d.ExternalIPServiceUrl == "" {
+		exitWithError("Please supply an External IP Service URL, e.g. " + externalIPService)
+	}
+
+	// request external ip address
+	resp, err := http.Get(d.ExternalIPServiceUrl)
+	if err != nil {
+		exitWithError(fmt.Sprintf("Failed trying External IP Service : %v", err.Error()))
+	}
+
+	if resp.StatusCode != 200 {
+		exitWithError(fmt.Sprintf("Failed trying External IP Service Response : %v", resp.StatusCode))
+	}
+
+	// close the response later
+	defer resp.Body.Close()
+
+	// get the whole contents or the response body
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		exitWithError(fmt.Sprintf("Failed to read response: %v", err.Error()))
+	}
+
+	// convert byte array to string and return
+	return strings.TrimSpace(string(contents))
+}
+
+// checks the ip address matches a valid IP regex
+func (d *dnsDodo) CheckIPV4(ip string) {
+	// check that the ip address string conforms to an IPV4 address
+	matches, err := regexp.MatchString(`^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`, ip)
+	if err != nil {
+		exitWithError(fmt.Sprintf("Error trying to query External IP Service [%s] : %s\n", d.ExternalIPServiceUrl, err))
+	}
+	if matches == false {
+		exitWithError(fmt.Sprintf("The ip address does not appear to be valid: %s\n", ip))
+	}
+}
+
+// Required Token interface for the oauth2 support
+// Sets up a token using the supplied personal access token
+func (d *dnsDodo) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: d.DOPersonalAccessToken,
+	}
+	return token, nil
+}
+
+// establishes a client connection to Digital Ocean API and stores it in the client property
+func (d *dnsDodo) EstablishGoDoClient() {
+	oauthClient := oauth2.NewClient(oauth2.NoContext, d)
+	d.client = godo.NewClient(oauthClient)
+}
+
+// retrieves all the Domain Records registered with the Digital Ocean account
+// As the api is paged, this method collates all page results into a single list
+func (d *dnsDodo) GetDNSEntries(domainName string) []godo.DomainRecord {
+	list := []godo.DomainRecord{} // to hold the domain records
+	opts := &godo.ListOptions{}
+	for {
+		// get a page of domain records
+		if pageOfRecords, resp, err := d.client.Domains.Records(domainName, opts); err != nil {
+			exitWithError(err.Error())
+		} else {
+			// add the retrived records to the list
+			list = append(list, pageOfRecords...)
+
+			// if there is not a links or is the last page in the response then we are done
+			if resp.Links == nil || resp.Links.IsLastPage() {
+				break
+			}
+
+			// if there are no more api requests per hour then fail
+			if resp.Rate.Remaining == 0 {
+				exitWithError(fmt.Sprintf("No API requests remaining for this hour, %v used. Next request can be made at %v", resp.Rate.Limit, resp.Rate.Reset))
+			}
+
+			// get current Page and increment the request options to the next page
+			if page, err := resp.Links.CurrentPage(); err != nil {
+				exitWithError(err.Error())
+			} else {
+				opts.Page = page + 1
+			}
+		}
+
+	}
+	return list
+}
+
+func (d *dnsDodo) FilteredRecords(records []godo.DomainRecord, typeFilter, nameFilter string) []godo.DomainRecord {
+	result := []godo.DomainRecord{}
+	for _, record := range records {
+		if (typeFilter == "" || record.Type == typeFilter) && (nameFilter == "" || record.Name == nameFilter) {
+			result = append(result, record)
+		}
+	}
+	return result
+}
+
+func (d *dnsDodo) OutputDomainRecords(records []godo.DomainRecord) {
+	fmt.Println("ID\tName\tType\tData")
+	for _, record := range records {
+		fmt.Printf("%d\t%s\t%s\t%s\n", record.ID, record.Name, record.Type, record.Data)
+	}
+}
+
+func (d *dnsDodo) UpdateDNSEntry(domainName, subdomain, ipAddress string) {
+	d.CheckIPV4(ipAddress)
+
+	fmt.Printf("About to update %s.%s to %s\n", subdomain, domainName, ipAddress)
+	records := d.GetDNSEntries(domainName)
+	if len(records) == 0 { // Check we have some entires before filtering
+		exitWithError(fmt.Sprintf("The dodo failed to retrieve any DNS entries for the domain '%s'", domainName))
+	}
+	records = d.FilteredRecords(records, "A", subdomain)
+	if len(records) == 0 { // Check we have found a record
+		exitWithError(fmt.Sprintf("There does not seem to be a DNS entry of type 'A' that has a name of '%s' for the domain '%s'", subdomain, domainName))
+	}
+	if len(records) > 1 { // Check we have not found more than one matching record
+		exitWithError(fmt.Sprintf("There does not seem to be a DNS entry of type 'A' that has a name of '%s' for the domain '%s'", subdomain, domainName))
+	}
+
+	// get the single record to update
+	record := records[0]
+	if record.Name != subdomain || record.Type != "A" {
+		exitWithError(fmt.Sprintf("Expected the record %v to be the record to update", record))
+	}
+
+	// create an update record with the updated details
+	domainER := godo.DomainRecordEditRequest{
+		Type:     record.Type,
+		Name:     record.Name,
+		Priority: record.Priority,
+		Port:     record.Port,
+		Weight:   record.Weight,
+		Data:     ipAddress,
+	}
+
+	// update the specified record id
+	recordId := record.ID
+	_, _, err := d.client.Domains.EditRecord(domainName, recordId, &domainER)
+	if err != nil {
+		exitWithError(err.Error())
+	}
+	fmt.Println("Updated.")
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "dns-dodo"
+	app.Usage = "Dynamic DNS sub-domain updater for Digital Ocean."
+	app.Version = "1.0"
+
+	// setup the default flags that are optional
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "extip, x",
+			Usage: "[Optional] External IP service URL",
+			Value: externalIPService,
+		},
+	}
+
+	// add the commands to the application
+	app.Commands = []cli.Command{
+		// show the external IP address from the external IP service
+		{
+			Name:  "show-ip",
+			Usage: fmt.Sprintf("Display the external IP Address using the default provider [%v], otherwise specify via --extip", externalIPService),
+			Action: func(c *cli.Context) {
+				dnsDodo := NewDnsDoDO()
+				dnsDodo.ExternalIPServiceUrl = c.GlobalString("extip")
+				ip := dnsDodo.getExternalIP()
+				dnsDodo.CheckIPV4(ip)
+				fmt.Printf("%s returned from %s\n", ip, dnsDodo.ExternalIPServiceUrl)
+			},
+		},
+
+		// show the DNS entries on Digital Ocean
+		{
+			Name:  "show-dns",
+			Usage: "Display the DNS entries on the account associated with the specified Personal Access Token and the specified domain name",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "pat, t",
+					Usage: "[Required] Personal Access Token from the Digital Ocean API, https://cloud.digitalocean.com/settings/tokens/new",
+				},
+				cli.StringFlag{
+					Name:  "domain, d",
+					Usage: "[Required] Domain Name to update or list dns entries for",
+				},
+				cli.StringFlag{
+					Name:  "type",
+					Usage: "[Optional] Type of DNS record to display e.g A",
+				},
+				cli.StringFlag{
+					Name:  "name",
+					Usage: "[Optional] value of the DNS Name property to filter to e.g @ | sub-domain",
+				},
+			},
+			Action: func(c *cli.Context) {
+				if !c.IsSet("pat") {
+					exitWithError("Error: A Personal Access Token must be supplied using the --pat flag")
+				}
+
+				if !c.IsSet("domain") {
+					exitWithError("Error: A domain must be specified to retrieve the DNS records for using the --domain flag")
+				}
+
+				dnsDodo := NewDnsDoDO()
+				dnsDodo.DOPersonalAccessToken = c.String("pat")
+				dnsDodo.EstablishGoDoClient()
+				records := dnsDodo.GetDNSEntries(c.String("domain"))
+				records = dnsDodo.FilteredRecords(records, c.String("type"), c.String("name"))
+				dnsDodo.OutputDomainRecords(records)
+			},
+		},
+
+		// update a DNS entry
+		{
+			Name:  "update-dns",
+			Usage: "Updates the DNS entry type 'A' for the specified [domain] that has a name of [subdomain] to the external IP address",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "pat, t",
+					Usage: "[Required] Personal Access Token from the Digital Ocean API, https://cloud.digitalocean.com/settings/tokens/new",
+				},
+				cli.StringFlag{
+					Name:  "domain, d",
+					Usage: "[Required] Domain Name to update the DNS 'A' record for",
+				},
+				cli.StringFlag{
+					Name:  "sub-domain, s",
+					Usage: "Sub-domain of the Domain Name that will be updated with the IP address",
+				},
+			},
+			Action: func(c *cli.Context) {
+				if !c.IsSet("pat") {
+					exitWithError("Error: A Personal Access Token must be supplied using the --pat flag")
+				}
+
+				if !c.IsSet("domain") {
+					exitWithError("Error: A domain must be specified to update the DNS records for using the --domain flag")
+				}
+
+				if !c.IsSet("sub-domain") {
+					exitWithError("Error: A sub-domain must be specified to update the DNS records for using the --domain flag")
+				}
+
+				dnsDodo := NewDnsDoDO()
+				dnsDodo.ExternalIPServiceUrl = c.GlobalString("extip")
+				ip := dnsDodo.getExternalIP()
+				dnsDodo.CheckIPV4(ip) // check the ip is valid before we attempt to connect to Digital Ocean
+				dnsDodo.DOPersonalAccessToken = c.String("pat")
+				dnsDodo.EstablishGoDoClient()
+				dnsDodo.UpdateDNSEntry(c.String("domain"), c.String("sub-domain"), ip)
+			},
+		},
+
+		// go get the dodo from the dojo
+		{
+			Name:  "dodo",
+			Usage: "Probably pining for the fjords...",
+			Action: func(c *cli.Context) {
+				NewDnsDoDO().About()
+			},
+		},
+
+		// display the application version
+		{
+			Name:  "version",
+			Usage: "Display application version",
+			Action: func(c *cli.Context) {
+				fmt.Println(app.Version)
+			},
+		},
+	}
+
+	// if no commands are supplied just show the help
+	app.Action = func(c *cli.Context) {
+		cli.ShowAppHelp(c)
+	}
+
+	// go make those dodos extinct
+	app.Run(os.Args)
+}
