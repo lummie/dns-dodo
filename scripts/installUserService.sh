8b8ad7ef95e6f243a50e76c8fb1d8e47ceb053a1
John Morley <john.morley.93@gmail.com>
2017-02-15 20:34:42 +0000

    Fix user unit installer failing for uninitialised unit directory

--- scripts/installUserService.sh
+++ scripts/installUserService.sh
@@ -4,13 +4,15 @@ if [ -z "$GOPATH" ]; then
     exit 1
 fi
 
+UNIT_DIR=$HOME/.local/share/systemd/user
+mkdir -p $UNIT_DIR
 chmod +x $GOPATH/bin/dns-dodo
 
 # Ensure our user unit can depend on network-online
 systemctl --user link /usr/lib/systemd/system/network-online.target
 
 # Substitute GOPATH from unit template and install as a user unit
-sed -e "s|{GOPATH}|$GOPATH|" -e "/^#/d" dns-dodo-user.service.template > ~/.local/share/systemd/user/dns-dodo.service
+sed -e "s|{GOPATH}|$GOPATH|" -e "/^#/d" dns-dodo-user.service.template > $UNIT_DIR/dns-dodo.service
 systemctl --user daemon-reload
 
 echo "Run 'systemctl --user start dns-dodo' to start"
